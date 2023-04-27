d96318adaf46a3cfd8fa9b4dd67aa0df0356f8c3
victoria giorgi <derrotagiorgi2636@gmail.com>
2023-04-27 18:48:58 -0300

    Primer cambio proyecto javascript

---
+++ main.js
@@ -0,0 +1,24 @@
+let nombre= prompt("Ingresa nombre");
+let apellido= prompt("Ingresa apellido");
+
+if(nombre!= "" && apellido!=""){
+    let mensaje= " Bienvenido/a " + nombre  +" "+  apellido;
+    alert(mensaje);
+}else{
+    alert("Ingrese nombre y apellido")
+}
+
+
+for(let turno= 1;turno <= 10; turno ++){
+ let numero= prompt("Ingrese DNI")
+ alert("Su turno es el" + ""+ turno);
+}
+alert("No hay mas turnos")
+
+
+let encuentro=("Tiempo de espera para el turno")
+function tiempo(a, b ){
+   return 10 && 20; 
+}
+let resultadotiempo= espera (10 && 20); 
+console.log("Tiempo"+ resultadotiempo);
